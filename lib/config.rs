09456e6d6c6fba5318141fbd3eb54ae599a80f6c
robin <comfysage@isabelroses.com>
2025-06-29 14:08:42 +0200

    fix: config serialization issues - trace using default config - flag all fields as optional

--- lib/config.rs
+++ lib/config.rs
@@ -6,35 +6,55 @@ use crate::util::constants;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ConfigMain {
+    #[serde(default = "ConfigMain::default_wallpapers")]
     pub wallpapers: String,
+    #[serde(default = "ConfigMain::default_provider")]
     pub provider: String,
+    #[serde(default = "ConfigMain::default_recurse")]
     pub recurse: bool,
+    #[serde(default = "ConfigMain::default_fileformats")]
     pub fileformats: Vec<String>,
+}
+
+impl ConfigMain {
+    pub fn default_wallpapers() -> String {
+        format!("{}/.wallpapers", &*constants::HOME)
+    }
+    pub fn default_provider() -> String {
+        "swww img".to_string()
+    }
+    pub fn default_recurse() -> bool {
+        true
+    }
+    pub fn default_fileformats() -> Vec<String> {
+        vec![
+            "jpg".to_string(),
+            "jpeg".to_string(),
+            "png".to_string(),
+            "gif".to_string(),
+            "pnm".to_string(),
+            "tga".to_string(),
+            "tiff".to_string(),
+            "webp".to_string(),
+            "bmp".to_string(),
+        ]
+    }
 }
 
 impl Default for ConfigMain {
     fn default() -> Self {
         Self {
-            wallpapers: format!("{}/.wallpapers", &*constants::HOME),
-            provider: "swww".to_string(),
-            recurse: true,
-            fileformats: vec![
-                "jpg".to_string(),
-                "jpeg".to_string(),
-                "png".to_string(),
-                "gif".to_string(),
-                "pnm".to_string(),
-                "tga".to_string(),
-                "tiff".to_string(),
-                "webp".to_string(),
-                "bmp".to_string(),
-            ],
+            wallpapers: Self::default_wallpapers(),
+            provider: Self::default_provider(),
+            recurse: Self::default_recurse(),
+            fileformats: Self::default_fileformats(),
         }
     }
 }
 
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct Config {
+    #[serde(default)]
     pub main: ConfigMain,
 }
 
@@ -51,8 +71,12 @@ impl Config {
     }
     pub fn new() -> Result<Self> {
         let path = Self::path()?;
-        if let Ok(content) = std::fs::read_to_string(path) {
-            let config: Self = toml::from_str(&content).unwrap_or(Self::default());
+        if let Ok(content) = std::fs::read_to_string(&path) {
+            let config: Self = toml::from_str(&content).unwrap_or_else(|e| {
+                debug!("couldn't create config from {path}: {e}");
+                debug!("using default config");
+                Self::default()
+            });
             Ok(config)
         } else {
             Ok(Self::default())
